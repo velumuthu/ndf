72ca13343390fa136973f55f8043f0b7646dc682
VELU M <velumbalaji@gmail.com>
2025-08-17 07:23:48 +0000

    Error: Invalid src prop (https://placehold.c) on `next/image`, hostname

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'placehold.c',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
